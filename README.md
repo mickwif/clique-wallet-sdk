6fae92d5fdac546c2bae2cf1002778ee5ea6b858
MickWang <1244134672@qq.com>
2025-03-06 15:32:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clique Wallet SDK
+
+A Solana wallet SDK for Clique, providing React components and utilities for wallet integration.
---
+++ src/lib/index.ts
@@ -0,0 +1,7 @@
+export * from "./client";
+export * from "./context";
+export * from "./hooks";
+export * from "./modal";
+export * from "./LoginModal";
+export * from "./storage";
+export * from "./types";
